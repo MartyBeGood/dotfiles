f7cbc1e602e700da946f3eefceb80ab6f86e1b9e
Markus Nölle <markus.nolle@auctionet.com>
2022-10-13 08:43:43 +0200

    visual changes in nvim

--- dot_config/nvim/lua/plugin/lualine.lua
+++ dot_config/nvim/lua/plugin/lualine.lua
@@ -1,7 +1,7 @@
 require('lualine').setup({
     options = {
         -- theme = 'auto',
-        theme = require('ofirkai.statuslines.lualine').theme,
+        theme = 'gruvbox-baby',
         component_separators = { left = '', right = ''},
         section_separators = { left = '', right = ''}
     }

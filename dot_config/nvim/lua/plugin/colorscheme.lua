ee246dbe1a6dd98f650921fea8a77c47b97b3a84
Markus Nölle <markus.nolle@auctionet.com>
2022-10-14 11:19:02 +0200

    Nicer Lualine

--- dot_config/nvim/lua/plugin/colorscheme.lua
+++ dot_config/nvim/lua/plugin/colorscheme.lua
@@ -2,5 +2,6 @@
 vim.opt.background = "dark"
 vim.opt.cursorline = true
 vim.g.gruvbox_baby_telescope_theme = 1
+vim.g.gruvbox_baby_use_original_palette = true
 vim.cmd([[colorscheme gruvbox-baby]])
 
--- dot_config/nvim/lua/plugin/lualine.lua
+++ dot_config/nvim/lua/plugin/lualine.lua
@@ -1,8 +1,15 @@
 require('lualine').setup({
     options = {
-        -- theme = 'auto',
-        theme = 'gruvbox-baby',
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''}
+        theme = 'gruvbox',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { { 'filename', path = 1 } },
+        lualine_x = { 'encoding', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
     }
 })

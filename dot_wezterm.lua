8e5728b1907bebe280f4f852c84014f2be41111b
Markus Nölle <markus.nolle@auctionet.com>
2022-10-18 16:28:53 +0200

    wezterm: make character under cursor readable

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,6 +1,11 @@
 return {
   color_scheme = 'Gruvbox dark, medium (base16)',
   hide_tab_bar_if_only_one_tab = true,
+  use_fancy_tab_bar = false,
   send_composed_key_when_left_alt_is_pressed = true,
-  font_size = 14
+  font_size = 14,
+  colors = {
+    cursor_bg = '#ebdbb2',
+    cursor_fg = '#282828'
+  }
 }

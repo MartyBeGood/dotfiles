2c8936c6986954005cc34b64da2955a7211667e3
Markus Nölle <markus.nolle@auctionet.com>
2022-10-18 16:31:42 +0200

    zsh: add powerlevel10k theme

--- dot_config/zsh/user.zsh
+++ dot_config/zsh/user.zsh
@@ -14,6 +14,17 @@ antigen use oh-my-zsh
 antigen bundle git
 antigen bundle pip
 antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle zsh-users/zsh-completions
 
-antigen theme robbyrussell
+antigen theme romkatv/powerlevel10k
 antigen apply
+
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f $zsh_config_dir/p10k.zsh ]] || source $zsh_config_dir/p10k.zsh

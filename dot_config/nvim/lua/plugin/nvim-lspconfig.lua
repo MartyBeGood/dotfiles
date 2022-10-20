6b5d75fa46608b15d52a66335eefb5d27fae2ca6
Markus Nölle <markus.nolle@auctionet.com>
2022-10-20 14:03:37 +0200

    nvim: Don't use null-ls for now

--- dot_config/nvim/lua/plugin/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugin/nvim-lspconfig.lua
@@ -5,8 +5,6 @@ require("mason-lspconfig").setup()
 local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.stylua,
-    null_ls.builtins.completion.spell
   }
 })
 require("mason-null-ls").setup()

68125aacb84db6a957bf6af0ec80883ea10b44fb
Markus Nölle <markus.nolle@auctionet.com>
2022-11-07 09:23:50 +0100

    neovim: LSP-format options

--- dot_config/nvim/lua/core/options.lua
+++ dot_config/nvim/lua/core/options.lua
@@ -6,41 +6,41 @@
 -- See: https://neovim.io/doc/user/vim_diff.html
 -- [2] Defaults - *nvim-defaults*
 
-local g = vim.g       -- Global variables
-local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
+local g = vim.g -- Global variables
+local opt = vim.opt -- Set options (global/buffer/windows-scoped)
 
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
-opt.mouse = 'a'                       -- Enable mouse support
-opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
+opt.mouse = 'a' -- Enable mouse support
+opt.completeopt = 'menuone,noinsert,noselect' -- Autocomplete options
 opt.timeoutlen = 500 -- shorter timeout for which-key
 opt.modeline = false
 -----------------------------------------------------------
 -- Neovim UI
 -----------------------------------------------------------
-opt.number = true           -- Show line number
-opt.showmatch = true        -- Highlight matching parenthesis
-opt.matchtime = 0           -- ... but don't make slow jumps please
-opt.foldmethod = 'marker'   -- Enable folding (default 'foldmarker')
+opt.number = true -- Show line number
+opt.showmatch = true -- Highlight matching parenthesis
+opt.matchtime = 0 -- ... but don't make slow jumps please
+opt.foldmethod = 'marker' -- Enable folding (default 'foldmarker')
 -- opt.colorcolumn = '80'      -- Line length marker at 80 columns
-opt.splitright = true       -- Vertical split to the right
-opt.splitbelow = true       -- Horizontal split to the bottom
-opt.ignorecase = true       -- Ignore case letters when search
-opt.smartcase = true        -- Ignore lowercase for the whole pattern
-opt.linebreak = true        -- Wrap on word boundary
-opt.termguicolors = true    -- Enable 24-bit RGB colors
-opt.laststatus = 2            -- One statusline per window
+opt.splitright = true -- Vertical split to the right
+opt.splitbelow = true -- Horizontal split to the bottom
+opt.ignorecase = true -- Ignore case letters when search
+opt.smartcase = true -- Ignore lowercase for the whole pattern
+opt.linebreak = true -- Wrap on word boundary
+opt.termguicolors = true -- Enable 24-bit RGB colors
+opt.laststatus = 2 -- One statusline per window
 
 -----------------------------------------------------------
 -- Tabs, indent, whitespace
 -----------------------------------------------------------
-opt.expandtab = true        -- Use spaces instead of tabs
-opt.shiftwidth = 4          -- Shift 4 spaces when tab
-opt.tabstop = 4             -- 1 tab == 4 spaces
-opt.smartindent = true      -- Autoindent new lines
+opt.expandtab = true -- Use spaces instead of tabs
+opt.shiftwidth = 4 -- Shift 4 spaces when tab
+opt.tabstop = 4 -- 1 tab == 4 spaces
+opt.smartindent = true -- Autoindent new lines
 opt.list = true
-opt.listchars = {           -- Show whitespace
+opt.listchars = { -- Show whitespace
   tab = '|-',
   extends = '⟩',
   precedes = '⟨',
@@ -50,11 +50,11 @@ opt.listchars = {           -- Show whitespace
 -----------------------------------------------------------
 -- Memory, CPU
 -----------------------------------------------------------
-opt.hidden = true           -- Enable background buffers
-opt.history = 100           -- Remember N lines in history
-opt.lazyredraw = true       -- Faster scrolling
-opt.synmaxcol = 240         -- Max column for syntax highlight
-opt.updatetime = 700        -- ms to wait for trigger an event
+opt.hidden = true -- Enable background buffers
+opt.history = 100 -- Remember N lines in history
+opt.lazyredraw = true -- Faster scrolling
+opt.synmaxcol = 240 -- Max column for syntax highlight
+opt.updatetime = 700 -- ms to wait for trigger an event
 
 -----------------------------------------------------------
 -- Startup
@@ -87,4 +87,3 @@ local disabled_built_ins = {
 for _, plugin in pairs(disabled_built_ins) do
   g["loaded_" .. plugin] = 1
 end
-

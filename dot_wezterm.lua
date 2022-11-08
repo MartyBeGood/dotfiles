eacb469c0845b0a4cd758eee9ccb89939eea17ad
Markus Nölle <markus.nolle@auctionet.com>
2022-11-08 18:15:33 +0100

    wezterm: fira code doesn't want to look good

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,11 +1,18 @@
+local wezterm = require 'wezterm'
 return {
-  color_scheme = 'Gruvbox dark, medium (base16)',
+  color_scheme = 'Monokai (base16)',
   hide_tab_bar_if_only_one_tab = true,
   use_fancy_tab_bar = false,
   send_composed_key_when_left_alt_is_pressed = true,
   font_size = 14,
   colors = {
-    cursor_bg = '#ebdbb2',
-    cursor_fg = '#282828'
+    cursor_bg = wezterm.color.get_default_colors().foreground,
+    cursor_fg = wezterm.color.get_default_colors().background
+  },
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 1,
+    bottom = 0,
   }
 }

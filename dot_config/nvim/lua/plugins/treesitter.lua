e8202207ec557bc3851a1acbbc9499806b8c206f
Markus Nölle <markus.nolle@auctionet.com>
2023-04-03 17:36:31 +0200

    nvim: Actually fix ruby indentation
    Turns out `end` is a valid method name too, so Treesitter tries to align
    A method call with its source.

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -8,11 +8,15 @@ return {
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
       highlight = {
-        -- `false` will disable the whole extension
         enable = true,
+        additional_vim_regex_highlighting = { 'ruby' }, -- Needed for non-treesitter indentation to work
       },
       indent = {
-        enable = false,
+        enable = true,
+        -- Disabling treesitter-indentation until either of the following tickets is resolved:
+        -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3363
+        -- https://github.com/tree-sitter/tree-sitter-ruby/issues/230
+        disable = { 'ruby' }
       },
       autotag = {
         enable = true,

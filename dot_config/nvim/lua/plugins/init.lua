8e1fadeeb3bced067c0acd1d9e590a149c224a8d
Markus Nölle <markus.nolle@auctionet.com>
2023-06-14 12:01:34 +0200

    nvim: Enable vim-test on more fts

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -21,12 +21,13 @@ return {
   },
   {
     "vim-test/vim-test",
-    ft = 'ruby',
+    ft = { 'ruby', 'js', 'ts', 'go' },
     config = function()
       vim.g['test#strategy'] = 'toggleterm'
       vim.g['test#ruby#rspec#options'] = {
         file = '--format documentation',
       }
+      vim.g['test#go#runner'] = 'gotest'
     end,
     dependencies = 'akinsho/toggleterm.nvim',
   },

bb664cb69a4ce76e9cb02390c66e295c54f6cab7
Markus Nölle <markus.nolle@auctionet.com>
2023-06-22 17:00:41 +0200

    zsh: Let's start over

--- dot_config/zsh/user.zsh
+++ dot_config/zsh/user.zsh
@@ -8,9 +8,6 @@ export EDITOR=nvim
 # common functions
 [ -f ~/.functions ] && source ~/.functions
 
-# plugins
-[ -f $zsh_config_dir/antigen.zsh ] && source $zsh_config_dir/antigen.zsh
-
 # brew completion
 if type brew &>/dev/null
 then
@@ -29,23 +26,4 @@ EOF
   fi
 }
 
-antigen use oh-my-zsh
-
-antigen bundle git
-antigen bundle pip
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle zsh-users/zsh-completions
-
-antigen apply
-
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
 eval "$(~/.local/bin/rtx activate zsh)"
-
-# rtx isn't ready for the rest of this file
-eval "$(rtx x starship -- starship init zsh)"

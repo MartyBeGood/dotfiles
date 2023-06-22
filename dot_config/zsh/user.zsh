9b18e2dcb6d392778467e12df7a333814dc27d80
Markus Nölle <markus.nolle@auctionet.com>
2023-06-22 23:22:45 +0200

    zsh: Restructure
    - p10k pure-style prompt
    - load rtx if available on path

--- dot_config/zsh/user.zsh
+++ dot_config/zsh/user.zsh
@@ -1,6 +1,25 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
 zsh_config_dir=${0:a:h}
-# basic env vars
+
+# env vars
 export EDITOR=nvim
+export PATH=$HOME/.local/bin:$PATH
+
+# plugin management
+source $zsh_config_dir/antigen.zsh
+antigen use oh-my-zsh
+antigen bundle zsh-users/zsh-syntax-highlighting
+# antigen bundle marlonrichert/zsh-autocomplete --branch=main
+antigen theme romkatv/powerlevel10k
+
+antigen apply
+# end plugin management
 
 # fzf
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
@@ -26,4 +45,9 @@ EOF
   fi
 }
 
-eval "$(~/.local/bin/rtx activate zsh)"
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+
+if [ command -v 'rtx' &> /dev/null ]; then
+  eval "$(rtx activate zsh)"
+fi

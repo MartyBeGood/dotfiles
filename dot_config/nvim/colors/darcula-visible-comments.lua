27ca7b60258d190eda16a6d955521e0b4df00247
Markus Nölle <markus.nolle@auctionet.com>
2023-07-03 10:12:10 +0200

    nvim: Leave unimportant Highlight groups grey
    There were a lot of HL groups that are meant to be background noise
    linked to 'Comment'. Have these in the original, unobtrusive color,
    while keeping meaningful comments visible.

--- dot_config/nvim/colors/darcula-visible-comments.lua
+++ dot_config/nvim/colors/darcula-visible-comments.lua
@@ -1,14 +1,70 @@
 vim.opt.background = "dark"
 vim.g.colors_name = 'darcula-visible-comments'
 
+-- First we define a group that should be present early on and should be grey
+vim.cmd("highlight default link diffComment NONE")
+
 local lush = require("lush")
 local darcula_solid = require("lush_theme.darcula-solid")
 local spec = lush.extends({ darcula_solid }).with(function()
-  local comment_color = lush.hsl(207, 100, 82)
+  local blue_comment = lush.hsl(207, 100, 82)
+  local gray_comment = lush.hsl(0, 0, 54) -- the original comment color
 
   return {
-    Comment { fg = comment_color, gui = "italic" },
+    Comment { fg = blue_comment, gui = "italic" },
+    diffComment { fg = gray_comment, gui = "italic" },
   }
 end)
 
 lush(spec)
+
+-- Then we link everything else to that one
+local gray_comment_groups = {
+  "CopilotSuggestion",
+  "CopilotAnnotation",
+  "cssComment",
+  "cssVendor",
+  "cssHacks",
+  "DiagnosticUnnecessary",
+  "dtdComment",
+  "dtdParamEntityDPunct",
+  "gitcommitComment",
+  "gitcommitOnBranch",
+  "gitcommitArrow",
+  "gitcommitDiscardedArrow",
+  "gitcommitSelectedArrow",
+  "gitcommitUnmergedArrow",
+  "hamlComment",
+  "hamlHtmlComment",
+  "htmlComment",
+  "htmlCssStyleComment",
+  "javaScriptLineComment",
+  "javaScriptComment",
+  "LspInfoTip",
+  "markdownBlockquote",
+  "rubyRegexpComment",
+  "rubyComment",
+  "rubyDocumentation",
+  "rubyData",
+  "sassComment",
+  "sassCssComment",
+  "sassEndOfLineComment",
+  "slimComment",
+  "TelescopeResultsComment",
+  "TroubleCode",
+  "TroubleSource",
+  "vbComment",
+  "vbLineLabel",
+  "vbLineNumber",
+  "WhichKeySeparator",
+  "WhichKeyValue",
+  "xmlAttribPunct",
+  "xmlComment",
+  "xmlCommentStart",
+  "xmlCommentPart",
+  "xmlProcessingDelim",
+  "@comment",
+}
+for _, group in ipairs(gray_comment_groups) do
+  vim.cmd("highlight default link " .. group .. " diffComment")
+end

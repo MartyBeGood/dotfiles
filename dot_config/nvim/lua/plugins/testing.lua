46d5305ade4d2d57b124aa181361bae3457be59a
Markus Nölle <markus.nolle@auctionet.com>
2023-09-15 10:29:26 +0200

    nvim: prefix test keybinds with space

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -17,10 +17,10 @@ return {
               local local_rspec = "./bin/rspec"
               if vim.fn.executable(local_rspec) == 1 then
                 return local_rspec
-              else
-                return vim.tbl_flatten({ "bundle", "exec", "rspec" })
               end
-            end,
+
+              return vim.tbl_flatten({ "bundle", "exec", "rspec" })
+            end
           })
         }
       }
@@ -31,16 +31,18 @@ return {
       neotest.setup(opts)
 
       require("which-key").register({
-        t = {
-          name = "Tests...",
-          a = { function() neotest.run.attach() end, "Show output of running test" },
-          f = { function() neotest.run.run(vim.fn.expand("%")) end, "Run all in file" },
-          l = { function() neotest.run.run_last() end, "Run last" },
-          o = { function() neotest.output.open() end, "Test Output in floating window" },
-          p = { function() neotest.output_panel.toggle() end, "Test Output in panel" },
-          s = { function() neotest.summary.toggle() end, "Test Summary panel" },
-          t = { function() neotest.run.run() end, "Run nearest" },
-        },
+        ["<space>"] = {
+          t = {
+            name = "Tests...",
+            a = { function() neotest.run.attach() end, "Show output of running test" },
+            f = { function() neotest.run.run(vim.fn.expand("%")) end, "Run all in file" },
+            l = { function() neotest.run.run_last() end, "Run last" },
+            o = { function() neotest.output.open() end, "Test Output in floating window" },
+            p = { function() neotest.output_panel.toggle() end, "Test Output in panel" },
+            s = { function() neotest.summary.toggle() end, "Test Summary panel" },
+            t = { function() neotest.run.run() end, "Run nearest" },
+          }
+        }
       })
     end
   }

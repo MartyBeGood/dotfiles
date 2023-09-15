62b87262f2f02e2000e9c8d12774165ecbb0aea3
Markus Nölle <markus.nolle@auctionet.com>
2023-09-15 10:13:57 +0200

    nvim: Remove vim-test
    All the specs I run so far have neotest adapters

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -1,19 +1,5 @@
 return {
   {
-    "vim-test/vim-test",
-    cond = not vim.g.vscode,
-    ft = { 'ruby', 'js', 'ts', 'go' },
-    config = function()
-      vim.g['test#strategy'] = 'toggleterm'
-      vim.g['test#ruby#rspec#options'] = {
-        file = '--format documentation',
-      }
-      vim.g['test#go#runner'] = 'gotest'
-    end,
-    dependencies = 'akinsho/toggleterm.nvim',
-  },
-
-  {
     "nvim-neotest/neotest",
     cond = not vim.g.vscode,
     ft = { 'ruby', 'js', 'ts', 'go' },

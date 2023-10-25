89a7bd5d789e795beff69d081efe2609c8521b6f
Markus Nölle <markus.nolle@auctionet.com>
2023-10-25 11:07:51 +0200

    nvim: config for indent-blankline v3

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,76 +1,73 @@
-if vim.g.vscode then
-  return {}
-else
-  return {
-    {
-      'nvim-lualine/lualine.nvim',
-      dependencies = {
-        'nvim-tree/nvim-web-devicons',
-      },
-      event = "UIEnter",
-      config = function()
-        require('lualine').setup({
-          options = {
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
-            disabled_filetypes = {
-              'NvimTree'
-            }
-          },
-          sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { { 'filename', path = 1 } },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
-          },
-        })
-      end
+return {
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
     },
-    {
-      'folke/noice.nvim',
-      dependencies = {
-        'MunifTanjim/nui.nvim'
-      },
-      opts = {
-        cmdline = {
-          enabled = true,
-          view = "cmdline"
-        },
-        messages = {
-          enabled = true,
-        },
-        lsp = {
-          override = {
-            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            ['vim.lsp.util.stylize_markdown'] = true,
-            ['cmp.entry.get_documentation'] = true,
-          },
-          hover = {
-            enabled = true,
-            view = nil
-          },
-          signature = {
-            enabled = true,
-            view = "hover"
+    event = "UIEnter",
+    config = function()
+      require('lualine').setup({
+        options = {
+          component_separators = { left = '', right = '' },
+          section_separators = { left = '', right = '' },
+          disabled_filetypes = {
+            'NvimTree'
           }
         },
-        presets = {
-          long_message_to_split = true
-        }
-      }
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = { 'branch', 'diff', 'diagnostics' },
+          lualine_c = { { 'filename', path = 1 } },
+          lualine_x = { 'encoding', 'fileformat', 'filetype' },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        },
+      })
+    end
+  },
+  {
+    'folke/noice.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim'
     },
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      event = "BufReadPre",
-      opts = {
+    opts = {
+      cmdline = {
+        enabled = true,
+        view = "cmdline"
+      },
+      messages = {
+        enabled = true,
+      },
+      lsp = {
+        override = {
+          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+          ['vim.lsp.util.stylize_markdown'] = true,
+          ['cmp.entry.get_documentation'] = true,
+        },
+        hover = {
+          enabled = true,
+          view = nil
+        },
+        signature = {
+          enabled = true,
+          view = "hover"
+        }
+      },
+      presets = {
+        long_message_to_split = true
+      }
+    }
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "BufReadPre",
+    main = "ibl",
+    opts = {
+      indent = {
         char = "┆",
-        context_char = "┆",
-        use_treesitter = false,
-        show_current_context = true,
-        show_current_context_start = true,
-        filetype_exclude = {
+      },
+      exclude = {
+        filetypes = {
           'help',
           'dashboard',
           'git',
@@ -81,7 +78,7 @@ else
           'packer',
           'NvimTree',
         },
-        buftype_exclude = {
+        buftypes = {
           'terminal',
           'nofile',
           'TelescopePrompt'
@@ -89,4 +86,4 @@ else
       }
     }
   }
-end
+}

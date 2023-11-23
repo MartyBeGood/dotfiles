4b821215971adbecc66b41e9948f76a09ccb5d8e
Markus Nölle <markus.nolle@auctionet.com>
2023-11-23 22:36:33 +0100

    nvim: neotest -> vim-test
    next step in trying to find the random slowdowns.
    Also I'm tired of waiting for test case parsing.

--- dot_config/nvim/lua/plugins/testing.lua
+++ dot_config/nvim/lua/plugins/testing.lua
@@ -1,45 +1,25 @@
 return {
   {
-    "nvim-neotest/neotest",
-    ft = { 'ruby', 'js', 'ts', 'go' },
+    "vim-test/vim-test",
     dependencies = {
-      'olimorris/neotest-rspec',
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
+      'akinsho/toggleterm.nvim',
+      'tpope/vim-dispatch'
     },
-
-    opts = function()
-      return {
-        adapters = {
-          require("neotest-rspec")({
-            rspec_cmd = function()
-              local local_rspec = "./bin/rspec"
-              if vim.fn.executable(local_rspec) == 1 then
-                return local_rspec
-              end
-
-              return vim.tbl_flatten({ "bundle", "exec", "rspec" })
-            end
-          })
-        }
+    config = function()
+      vim.g["test#strategy"] = "dispatch"
+      vim.g['test#ruby#rspec#options'] = {
+        file = '--format documentation'
       }
-    end,
 
-    config = function(_, opts)
-      local neotest = require("neotest")
-      neotest.setup(opts)
-
+      local cmdify = require('helpers').cmdify
       require("which-key").register({
         ["<space>"] = {
           t = {
             name = "Tests...",
-            a = { function() neotest.run.attach() end, "Show output of running test" },
-            f = { function() neotest.run.run(vim.fn.expand("%")) end, "Run all in file" },
-            l = { function() neotest.run.run_last() end, "Run last" },
-            o = { function() neotest.output.open() end, "Test Output in floating window" },
-            p = { function() neotest.output_panel.toggle() end, "Test Output in panel" },
-            s = { function() neotest.summary.toggle() end, "Test Summary panel" },
-            t = { function() neotest.run.run() end, "Run nearest" },
+            f = { cmdify("TestFile"), "Run all in file" },
+            l = { cmdify("TestLast"), "Run last" },
+            s = { cmdify("TestSuite"), "Run test suite" },
+            t = { cmdify("TestNearest"), "Run nearest" },
           }
         }
       })

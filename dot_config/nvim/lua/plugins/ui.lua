4a2111ce6f7a548850c67730686af2c84fa8e4a7
Markus Nölle <markus.nolle@auctionet.com>
2023-11-23 22:35:14 +0100

    nvim: display which lsp servers are attached

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -17,66 +17,35 @@ return {
         sections = {
           lualine_a = { 'mode' },
           lualine_b = { 'branch', 'diff', 'diagnostics' },
-          lualine_c = { { 'filename', path = 1 } },
+          lualine_c = {
+            { 'filename', path = 1 },
+            {
+              function()
+                local msg = 'No Active Lsp'
+                local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                local clients = vim.lsp.get_active_clients()
+
+                local server_names = {}
+                for _, client in ipairs(clients) do
+                  local filetypes = client.config.filetypes
+                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    table.insert(server_names, client.name)
+                  end
+                end
+
+                if next(server_names) == nil then
+                  return ''
+                end
+
+                return '󰒋 ' .. table.concat(server_names, ', ')
+              end,
+            }
+          },
           lualine_x = { 'encoding', 'fileformat', 'filetype' },
           lualine_y = { 'progress' },
           lualine_z = { 'location' }
         },
       })
     end
-  },
-  {
-    'folke/noice.nvim',
-    dependencies = {
-      'MunifTanjim/nui.nvim'
-    },
-    opts = {
-      cmdline = {
-        enabled = false,
-      },
-      messages = {
-        enabled = false,
-      },
-      popupmenu = {
-        enabled = false,
-      },
-      lsp = {
-        override = {
-          ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-
-          ['vim.lsp.util.stylize_markdown'] = true,
-          ['cmp.entry.get_documentation'] = true,
-        },
-        hover = {
-          enabled = true,
-          view = nil
-        },
-        signature = {
-          enabled = true,
-          view = "hover"
-        }
-      },
-      presets = {
-        long_message_to_split = true
-      },
-      routes = {
-        {
-          filter = {
-            event = "msg_show",
-            kind = "",
-            find = "B written"
-          },
-          opts = { skip = true }
-        }
-      },
-      views = {
-        mini = {
-          position = {
-            row = -2,
-            col = "100%"
-          }
-        }
-      }
-    }
   },
 }

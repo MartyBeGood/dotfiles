3117c9b8f6e3561a2a9cc3ce19361114c0ca527b
Markus Nölle <markus.nolle@auctionet.com>
2024-03-13 09:13:56 +0100

    nvim: Re-add luasnip
    I don't care much about snippets, but cmp needs some snippet engine to
    expand the LSP-provided snippets. Emmet completely breaks without a
    snippet engine and I like emmet.

--- dot_config/nvim/lua/plugins/autocomplete.lua
+++ dot_config/nvim/lua/plugins/autocomplete.lua
@@ -25,6 +25,8 @@ return {
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
       'onsails/lspkind.nvim',
     },
     config = function()
@@ -32,6 +34,11 @@ return {
 
       cmp.setup({
         preselect = cmp.PreselectMode.None,
+        snippet = {
+          expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+          end,
+        },
         completion = {
           completeopt = 'menu,menuone,noinsert,noselect',
         },
@@ -47,6 +54,7 @@ return {
         },
         sources = {
           { name = 'nvim_lsp' },
+          { name = 'luasnip' },
           { name = 'path' },
           { name = 'buffer' },
         },

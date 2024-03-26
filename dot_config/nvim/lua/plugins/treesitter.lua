c1763f81d288e8f7266d381760c1a6a8a375f94e
Markus Nölle <markus.nolle@auctionet.com>
2024-03-26 17:12:03 +0100

    more bg-switching prep

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,41 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-
-    main = 'nvim-treesitter.configs', -- Which module to call '#setup(opts)' on
-    opts = {
-      auto_install = true,
-      sync_install = false, -- Don't install parsers synchronously (only applied to `ensure_installed`)
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = { 'markdown', 'gitcommit' }, -- TS seems to take away highlighting from these filetypes
-      },
-      indent = {
-        enable = true,
-        -- Disabling treesitter-indentation until either of the following tickets is resolved:
-        -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3363
-        -- https://github.com/tree-sitter/tree-sitter-ruby/issues/230
-        disable = { 'ruby', 'html' }
-      },
-      autotag = {
-        enable = true,
-      },
-      endwise = {
-        enable = true,
-      }
-    },
+    build = ":TSUpdate",
 
     dependencies = {
       "RRethy/nvim-treesitter-endwise",
       "windwp/nvim-ts-autotag",
-    }
+    },
+
+    config = function()
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+        auto_install = true,
+        ensure_installed = { "c", "lua", "vimdoc", "query" },
+        ignore_install = {},
+        modules = {},
+        sync_install = false, -- Don't install parsers synchronously (only applied to `ensure_installed`)
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = { 'markdown', 'gitcommit' }, -- TS seems to take away highlighting from these filetypes
+        },
+        indent = {
+          enable = true,
+          -- Disabling treesitter-indentation until either of the following tickets is resolved:
+          -- https://github.com/nvim-treesitter/nvim-treesitter/issues/3363
+          -- https://github.com/tree-sitter/tree-sitter-ruby/issues/230
+          disable = { 'ruby' }
+        },
+        autotag = {
+          enable = true,
+        },
+        endwise = {
+          enable = true,
+        }
+      })
+    end,
+
   }
 }

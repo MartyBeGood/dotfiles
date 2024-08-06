da1b15b27afb04f22bd3d2a78a19930286211a1e
Markus Nölle <markus.nolle@auctionet.com>
2024-08-06 20:47:27 +0200

    nvim: sane defaults for neotree
    Just show hidden files pls. You're a file-EXPLORER

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -24,22 +24,29 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     dependencies = {
-      {
-        's1n7ax/nvim-window-picker',
-        name = 'window-picker',
-        event = 'VeryLazy',
-        version = '2.*',
-        opts = {
-          hint = 'floating-big-letter',
-          show_prompt = false,
-
-        }
-      }
+      's1n7ax/nvim-window-picker',
     },
     opts = {
       window = {
         position = "right",
       },
+      filesystem = {
+        filtered_items = {
+          visible = true
+        }
+      }
     },
   },
+
+  {
+    's1n7ax/nvim-window-picker',
+    name = 'window-picker',
+    event = 'VeryLazy',
+    version = '2.*',
+    opts = {
+      hint = 'floating-big-letter',
+      show_prompt = false,
+
+    }
+  }
 }

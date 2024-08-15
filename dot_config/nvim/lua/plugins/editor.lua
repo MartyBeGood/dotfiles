f87961c7753c01584abc16987b4fe5475d15b398
Markus Nölle <markus.nolle@auctionet.com>
2024-08-15 08:22:26 +0200

    nvim: formatter

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -2,7 +2,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     dependencies = {
-      's1n7ax/nvim-window-picker',
+      "s1n7ax/nvim-window-picker",
     },
     opts = {
       window = {
@@ -10,23 +10,22 @@ return {
       },
       filesystem = {
         filtered_items = {
-          visible = true
-        }
-      }
+          visible = true,
+        },
+      },
     },
   },
 
   {
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    event = 'VeryLazy',
-    version = '2.*',
+    "s1n7ax/nvim-window-picker",
+    name = "window-picker",
+    event = "VeryLazy",
+    version = "2.*",
     opts = {
-      hint = 'floating-big-letter',
+      hint = "floating-big-letter",
       show_prompt = false,
-
-    }
-  }
+    },
+  },
   {
     "kylechui/nvim-surround",
     event = "VeryLazy",

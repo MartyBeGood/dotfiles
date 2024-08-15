949c802a59d43edfc6eb329a971b8c76e813d603
Markus Nölle <markus.nolle@auctionet.com>
2024-08-15 08:23:19 +0200

    nvim: don't truncate path in statusline

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -1,54 +1,68 @@
 return {
   {
     "nvim-lualine/lualine.nvim",
-    opts = {
-      options = {
-        component_separators = { left = "", right = "" },
-        section_separators = { left = "", right = "" },
-        globalstatus = false,
-      },
-      sections = {
-        lualine_x = {
-          {
-            function()
-              local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-              local clients = vim.lsp.get_active_clients()
+    opts = function(_, default_opts)
+      local my_opts = {
+        options = {
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          globalstatus = false,
+          length = 0,
+        },
+        sections = {
+          lualine_x = {
+            {
+              function()
+                local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+                local clients = vim.lsp.get_active_clients()
 
-              local server_names = {}
-              for _, client in ipairs(clients) do
-                local filetypes = client.config.filetypes
-                if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-                  table.insert(server_names, client.name)
+                local server_names = {}
+                for _, client in ipairs(clients) do
+                  local filetypes = client.config.filetypes
+                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    table.insert(server_names, client.name)
+                  end
                 end
-              end
 
-              if next(server_names) == nil then
-                return ""
-              end
+                if next(server_names) == nil then
+                  return ""
+                end
 
-              return "󰒋 " .. table.concat(server_names, ", ")
-            end,
+                return "󰒋 " .. table.concat(server_names, ", ")
+              end,
+            },
+          },
+          lualine_y = {
+            { "encoding" },
+            { "fileformat" },
+          },
+          lualine_z = {
+            {
+              "progress",
+              separator = " ",
+              padding = { left = 1, right = 0 },
+            },
+            {
+              "location",
+              separator = " ",
+              padding = { left = 0, right = 1 },
+            },
+            { "selectioncount" },
           },
         },
-        lualine_y = {
-          { "encoding" },
-          { "fileformat" },
-        },
-        lualine_z = {
-          {
-            "progress",
-            separator = " ",
-            padding = { left = 1, right = 0 },
-          },
-          {
-            "location",
-            separator = " ",
-            padding = { left = 0, right = 1 },
-          },
-          { "selectioncount" },
-        },
-      },
-    },
+      }
+
+      local opts = vim.tbl_deep_extend("force", default_opts, my_opts)
+
+      table.remove(opts.sections.lualine_c, table.maxn(opts.sections.lualine_c) - 1)
+      table.insert(
+        opts.sections.lualine_c,
+        table.maxn(opts.sections.lualine_c),
+        { LazyVim.lualine.pretty_path({ length = 0 }) }
+      )
+
+      return opts
+    end,
   },
 
   {

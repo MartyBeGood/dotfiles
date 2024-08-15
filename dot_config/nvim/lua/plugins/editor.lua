15cff1d48fc8cd4b90e8f4e8c5a819e050f96401
Markus Nölle <markus.nolle@auctionet.com>
2024-08-15 08:08:02 +0200

    nvim: mini.surround -> surround.nvim

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,27 +1,5 @@
 return {
   {
-    "folke/flash.nvim",
-    keys = {
-      -- these would conflict with surround.nvim, and I don't use them anyway
-      { "s", mode = { "n", "x", "o" }, false },
-      { "S", mode = { "n", "o", "x" }, false }
-    }
-  },
-  {
-    "echasnovski/mini.surround",
-    opts = {
-      mappings = {
-        add = "sa",
-        delete = "sd",
-        find = "sf",
-        find_left = "sF",
-        highlight = "sh",
-        replace = "sr",
-        update_n_lines = "sn"
-      }
-    }
-  },
-  {
     "nvim-neo-tree/neo-tree.nvim",
     dependencies = {
       's1n7ax/nvim-window-picker',
@@ -49,4 +27,9 @@ return {
 
     }
   }
+  {
+    "kylechui/nvim-surround",
+    event = "VeryLazy",
+    config = true,
+  },
 }

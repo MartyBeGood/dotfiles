4fde215ad9b44399481d38a1b2bd5953af42bcc6
Markus Nölle <markus.nolle@auctionet.com>
2024-10-30 16:06:06 +0100

    Add starship pure config & bash init

--- dot_bashrc_user.sh
+++ dot_bashrc_user.sh
@@ -6,16 +6,23 @@ export EDITOR=nvim
 # common functions
 [ -f ~/.functions ] && source ~/.functions
 
+# common aliases
+[ -f ~/.aliases ] && source ~/.aliases
+
 # fzf
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
-
 
 if [ -f ~/.local/bin/mise ]; then
   eval "$(~/.local/bin/mise activate bash)"
 else
-  if [ command -v 'mise' &> /dev/null ]; then
+  if [ command -v 'mise' ] &>/dev/null; then
     eval "$(mise activate bash)"
   else
     echo "mise not available. chezmoi setup might be borked."
   fi
+fi
+
+# load starship prompt if the command exists
+if [ -x "$(command -v starship)" ]; then
+  eval "$(starship init bash)"
 fi

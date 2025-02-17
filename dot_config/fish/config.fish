2379c4116b48c7314fbb9c4adf2938070668f819
Markus Nölle <markus.nolle@auctionet.com>
2025-02-17 09:47:29 +0100

    fish: remove
    I'm not using fish anymore and I messed up the config so much that
    chezmoi started complaining about it.

--- dot_config/fish/config.fish
+++
@@ -1,6 +0,0 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-    if test -e (which starship)
-        starship init fish | source
-    end
-end
--- dot_config/fish/functions/ta.fish
+++
@@ -1,7 +0,0 @@
-function ta
-    if test -n "$argv[1]"
-        tmux attach -t $argv[1]
-    else
-        tmux attach
-    end
-end

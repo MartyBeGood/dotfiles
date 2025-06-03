19df509a2cb6c050f74d35924d73adab8bb8fcbb
Markus Nölle <markus.nolle@auctionet.com>
2025-06-03 11:25:21 +0200

    nvim: Add gitsigns
    line blame and change markers are nice

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,3 +1,10 @@
 return {
-  { "tpope/vim-fugitive", cond = not vim.g.vscode }
+  { "tpope/vim-fugitive", cond = not vim.g.vscode },
+  {
+    "lewis6991/gitsigns.nvim",
+    cond = not vim.g.vscode,
+    opts = {
+      current_line_blame = true
+    }
+  }
 }

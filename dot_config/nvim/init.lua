0ac8f3f309fadacb2069d2d16b0c46ec5cd5791a
Markus Nölle <markus.noelle@gmail.com>
2025-06-13 11:21:30 +0200

    nvim: Remove redundant toggle definition
    They're in the setup for conform anyway, so 🤷

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,16 +1,2 @@
 require("config.core_options")
 require("config.lazy")
-
-Snacks.toggle.new({
-  id = "format_on_save_global",
-  name = "Format on save (global)",
-  get = function() return not vim.g.disable_autoformat; end, -- reversing avoids defining the global/buffer-local variable ahead of time
-  set = function(state) vim.g.disable_autoformat = not state; end,
-}):map("<leader>uF")
-
-Snacks.toggle.new({
-  id = "format_on_save_buffer",
-  name = "Format on save (buffer)",
-  get = function() return not vim.b.disable_autoformat; end,
-  set = function(state) vim.b.disable_autoformat = not state; end,
-}):map("<leader>uf")

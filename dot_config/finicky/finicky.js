d79d52c6fdb476460b2d5ea0a871a85840c125a5
Markus Nölle <markus.noelle@gmail.com>
2025-06-19 13:26:02 +0200

    Finicky: Open everything that doesn't have a PWA in Firefox

---
+++ dot_config/finicky/finicky.js
@@ -0,0 +1,13 @@
+export default {
+  defaultBrowser: "Firefox",
+  handlers: [
+    {
+      match: "apple.com/*",
+      browser: "Safari",
+    },
+    {
+      match: "meet.google.com/*",
+      browser: "Safari",
+    },
+  ]
+}

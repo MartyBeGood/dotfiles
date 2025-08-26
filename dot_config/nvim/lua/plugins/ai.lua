878f4df0487bcac6af30752799310035fe3673fd
Markus Nölle <markus.nolle@auctionet.com>
2025-08-26 10:34:37 +0200

    nvim: AI, Lazyvim-style

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,19 +1,2 @@
 return {
-  {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    cond = true,
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<M-y>",
-          accept_line = "<M-l>",
-          next = "<M-n>",
-          prev = "<M-p>",
-        },
-      },
-    },
-  },
 }

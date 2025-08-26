8acd7893cc8e5026ee6149f1f5b9c98c69b50b1a
Markus Nölle <markus.nolle@auctionet.com>
2025-08-26 11:46:42 +0200

    nvim: fix bufferline

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -101,4 +101,15 @@ return {
       },
     },
   },
+
+  -- Remove this once https://github.com/LazyVim/LazyVim/pull/6354 is merged
+  {
+    "akinsho/bufferline.nvim",
+    init = function()
+      local bufline = require("catppuccin.groups.integrations.bufferline")
+      function bufline.get()
+        return bufline.get_theme()
+      end
+    end,
+  },
 }

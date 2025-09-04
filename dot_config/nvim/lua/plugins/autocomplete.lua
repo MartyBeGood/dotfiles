93bee3692315a5467a11c1c2fdd5e6e464fd0547
Markus Nölle <markus.nolle@auctionet.com>
2025-09-04 15:59:50 +0200

    nvim: remove luasnip config

--- dot_config/nvim/lua/plugins/autocomplete.lua
+++ dot_config/nvim/lua/plugins/autocomplete.lua
@@ -18,11 +18,4 @@ return {
       },
     },
   },
-  {
-    "L3MON4D3/LuaSnip",
-    -- Remove all keybindings. They're handled in nvim-cmp.
-    keys = function()
-      return {}
-    end,
-  },
 }

8c7dbb76749b13cbf32ee39b4a705a0bf65ee8a1
Markus Nölle <markus.nolle@auctionet.com>
2025-12-08 19:29:19 +0100

    nvim: remove codecompanion

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,11 +1,3 @@
 return {
   "github/copilot.vim",
-  {
-    "olimorris/codecompanion.nvim",
-    event = "VeryLazy",
-    opts = {},
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-  },
 }

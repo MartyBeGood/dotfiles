82451f68ea52c28db94d0caefea143a558ca439b
Markus Nölle <markus.nolle@auctionet.com>
2026-02-13 18:37:33 +0100

    nvim: Less annoying copilot maps

--- dot_config/nvim/lua/plugins/ai.lua
+++ dot_config/nvim/lua/plugins/ai.lua
@@ -1,3 +1,18 @@
 return {
   "github/copilot.vim",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+  end,
+  event = "VeryLazy",
+  keys = {
+    {
+      "<C-J>",
+      'copilot#Accept("\\<CR>")',
+      mode = "i",
+      desc = "Accept Copilot suggestion",
+      expr = true,
+      replace_keycodes = false,
+    },
+    { "<C-L>", "<Plug>(copilot-accept-line)", mode = "i", desc = "Accept current line of Copilot suggestion" },
+  },
 }

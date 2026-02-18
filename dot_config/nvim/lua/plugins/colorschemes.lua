175c939a5f92a1a8f665d6e7b502c3435b46e8f9
Markus Nölle <markus.nolle@auctionet.com>
2026-02-18 08:31:01 +0100

    nvim: monokai pro spectrum pls

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -21,20 +21,91 @@ return {
     "polirritmico/monokai-nightasty.nvim", -- Monokai with a neutral grey background.
     opts = {
       dim_inactive = false,
-      dark_style_background = "dark",
+
+      floats = "dark",
+      sidebars = "dark",
       on_colors = function(colors)
-        if vim.o.background == "light" then
-          colors.blue = "#0043c3"
-          colors.green = "#207000"
-          colors.magenta = "#d0325c"
-          colors.orange = "#cf3000"
-          colors.yellow = "#df6f00"
-          colors.git.add = "#30a000"
-          colors.git.change = "#df6f00"
-          colors.git.remove = "cf3000"
+        -- This function is run after the theme extracts all kinds of extra colors
+        local is_light = vim.o.background == "light"
+        if is_light then
+          colors.blue = "#428ba6"
+          colors.green = "#469c6c"
+          colors.magenta = "#d34d75"
+          colors.orange = "#d3643a"
+          colors.yellow = "#c17c23"
+          colors.purple = "#7257ba"
         else
-          colors.magenta = "#e34481"
+          colors.blue = "#5ad4e6" -- accent5
+          colors.green = "#7bd88f" -- accent4
+          colors.magenta = "#fc618d" -- accent1
+          colors.orange = "#fd9353" -- accent2
+          colors.purple = "#948ae3" -- accent6
+          colors.yellow = "#fce566" -- accent3
+
+          colors.blue_alt = "#5ad4e6"
+          colors.green_alt = "#7bd88f"
+          colors.green_light = "#a9dc76" -- (Classic Monokai Green for variety)
+          colors.red = "#fc618d" -- accent1
+          colors.yellow_alt = "#fce566"
         end
+
+        colors.git.add = colors.green -- accent4
+        colors.git.change = colors.blue -- accent2
+        colors.git.delete = colors.red -- accent1
+
+        -- copied from the theme itself to re-do the extraction
+        -- Default values for functions util.darken() and util.lighter()
+        local utils = require("monokai-nightasty.utils")
+        utils.bg = colors.bg
+        utils.fg = colors.fg
+
+        colors.border = colors.blue
+        colors.border_highlight = colors.fg
+
+        -- Statusline
+        colors.bg_statusline = is_light and utils.darken(colors.bg, 0.93, colors.fg) or colors.grey_darker
+        colors.fg_statusline = colors.fg_dark
+
+        -- Sidebar and Floats
+
+        -- Set the background for the current line (current cursor position)
+        colors.bg_highlight = utils.darken(colors.bg, 0.9, colors.fg) -- (0.97 for #313131)
+
+        colors.bg_visual = is_light and colors.charcoal or colors.grey_darker
+        colors.bg_search = colors.yellow
+        colors.fg_search = is_light and colors.white or colors.black
+        colors.fg_sidebar = colors.fg_dark
+
+        -- For lsp floats messages, git, diffs, etc.
+        colors.error = colors.red
+        colors.hint = colors.green_alt
+        colors.info = colors.blue_alt
+        colors.note = utils.blend(colors.orange, colors.yellow, 0.6)
+        colors.todo = colors.purple
+        colors.warning = colors.orange
+        colors.diff = {
+          add = utils.darken(colors.green, 0.15),
+          delete = utils.darken(colors.red, 0.15),
+          change = utils.darken(colors.blue_alt, 0.15),
+          text = utils.darken(colors.blue_alt, 0.3),
+        }
+
+        -- Lualine
+        colors.lualine = {
+          normal_bg = is_light and colors.green_light or colors.green,
+          normal_fg = is_light and colors.white or colors.black,
+          command_fg = is_light and utils.import_color("dark", "green") or colors.green,
+        }
+
+        -- Rainbow colors (for color_headers, rainbow delimiters and other plugins)
+        colors.rainbow = {
+          colors.fg,
+          colors.yellow,
+          colors.orange,
+          colors.red,
+          colors.purple,
+          colors.blue,
+        }
       end,
     },
   },

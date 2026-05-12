115c5e0ab84aacf3dcca2d588370a3ae51f71c91
Markus Nölle <markus.nolle@auctionet.com>
2026-05-12 16:21:11 +0200

    Add keymaps to move focus while in term mode

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,7 @@
 -- Add any additional keymaps here
 vim.keymap.del("n", "<leader>bb")
 vim.keymap.set("t", "<leader><Esc>", "<C-\\><C-n>")
+vim.keymap.set("t", "<C-w>h", "<C-\\><C-n><C-w>h")
+vim.keymap.set("t", "<C-w>j", "<C-\\><C-n><C-w>j")
+vim.keymap.set("t", "<C-w>k", "<C-\\><C-n><C-w>k")
+vim.keymap.set("t", "<C-w>l", "<C-\\><C-n><C-w>l")

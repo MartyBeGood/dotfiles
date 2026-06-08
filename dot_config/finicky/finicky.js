d10f782a312bb26fbfba527aedc8ada4aeb8a542
Markus Nölle <martybegood@gmx.com>
2026-06-08 14:15:14 +0200

    finicky: reshape

--- dot_config/finicky/finicky.js
+++ dot_config/finicky/finicky.js
@@ -1,110 +1,82 @@
-const workSlackTeamMapping = {
-  'auctionet': 'T7Q9KJ19B',
-  'linearcustomers': 'TM567NELB',
-};
+function rewriteSlack(subdomainMapping) {
+  return {
+    match: ["*.slack.com/*"],
+    url(urlObj) {
+      const subdomain = urlObj.host.slice(0, -10); // before .slack.com
 
-const personalSlackMapping = {
+      let team, patterns = {};
+      if (subdomain !== "app") {
+        if (!(subdomain in subdomainMapping)) {
+          console.log(
+            `No Slack team ID found for ${urlObj.host}`,
+            `Add a correct team ID to ~/.finicky.js to allow direct linking to Slack.`
+          );
+          return urlObj;
+        }
+        team = subdomainMapping[subdomain];
+
+        if (subdomain.slice(-11) === ".enterprise") {
+          patterns = {
+            file: [/\/files\/\w+\/(?<id>\w+)/],
+          };
+        } else {
+          patterns = {
+            file: [/\/messages\/\w+\/files\/(?<id>\w+)/],
+            team: [/(?:\/messages\/\w+)?\/team\/(?<id>\w+)/],
+            channel: [/\/(?:messages|archives)\/(?<id>\w+)(?:\/(?<message>p\d+))?/],
+          };
+        }
+      } else {
+        patterns = {
+          file: [
+            /\/client\/(?<team>\w+)\/\w+\/files\/(?<id>\w+)/,
+            /\/docs\/(?<team>\w+)\/(?<id>\w+)/,
+          ],
+          team: [/\/client\/(?<team>\w+)\/\w+\/user_profile\/(?<id>\w+)/],
+          channel: [/\/client\/(?<team>\w+)\/(?<id>\w+)(?:\/(?<message>[\d.]+))?/],
+        };
+      }
+
+      for (const [host, hostPatterns] of Object.entries(patterns)) {
+        for (const pattern of hostPatterns) {
+          const match = pattern.exec(urlObj.pathname);
+          if (match) {
+            let search = `team=${team || match.groups.team}`;
+            if (match.groups.id) search += `&id=${match.groups.id}`;
+            if (match.groups.message) {
+              let message = match.groups.message;
+              if (message[0] === "p") message = message.slice(1, 11) + "." + message.slice(11);
+              search += `&message=${message}`;
+            }
+            const outputStr = `slack://${host}?${search}`;
+            console.log(`Rewrote Slack URL ${urlObj.href} to deep link ${outputStr}`);
+            return new URL(outputStr);
+          }
+        }
+      }
+
+      return urlObj;
+    },
+  };
+}
+
+const slackSubdomainMapping = {
+  auctionet: "T7Q9KJ19B",
+  linearcustomers: "TM567NELB",
   // personal slacks
 };
 
-const slackSubdomainMapping = {
-  ...workSlackTeamMapping,
-  ...personalSlackMapping,
-};
-
-const slackRewriter = {
-  match: ["*.slack.com/*"],
-  url: function(urlObj) {
-    const subdomain = urlObj.host.slice(0, -10); // before .slack.com
-    const pathParts = urlObj.pathname.split("/");
-
-    let team,
-      patterns = {};
-    if (subdomain != "app") {
-      if (!Object.keys(slackSubdomainMapping).includes(subdomain)) {
-        console.log(
-          `No Slack team ID found for ${urlObj.host}`,
-          `Add a correct team ID to ~/.finicky.js to allow direct linking to Slack.`
-        );
-        return urlObj;
-      }
-      team = slackSubdomainMapping[subdomain];
-
-      if (subdomain.slice(-11) == ".enterprise") {
-        patterns = {
-          file: [/\/files\/\w+\/(?<id>\w+)/],
-        };
-      } else {
-        patterns = {
-          file: [/\/messages\/\w+\/files\/(?<id>\w+)/],
-          team: [/(?:\/messages\/\w+)?\/team\/(?<id>\w+)/],
-          channel: [
-            /\/(?:messages|archives)\/(?<id>\w+)(?:\/(?<message>p\d+))?/,
-          ],
-        };
-      }
-    } else {
-      patterns = {
-        file: [
-          /\/client\/(?<team>\w+)\/\w+\/files\/(?<id>\w+)/,
-          /\/docs\/(?<team>\w+)\/(?<id>\w+)/,
-        ],
-        team: [/\/client\/(?<team>\w+)\/\w+\/user_profile\/(?<id>\w+)/],
-        channel: [
-          /\/client\/(?<team>\w+)\/(?<id>\w+)(?:\/(?<message>[\d.]+))?/,
-        ],
-      };
-    }
-
-    for (let [host, host_patterns] of Object.entries(patterns)) {
-      for (let pattern of host_patterns) {
-        let match = pattern.exec(urlObj.pathname);
-        if (match) {
-          let search = `team=${team || match.groups.team}`;
-
-          if (match.groups.id) {
-            search += `&id=${match.groups.id}`;
-          }
-
-          if (match.groups.message) {
-            let message = match.groups.message;
-            if (message.charAt(0) == "p") {
-              message = message.slice(1, 11) + "." + message.slice(11);
-            }
-            search += `&message=${message}`;
-          }
-
-          let outputStr = `slack://${host}?${search}`;
-          console.log(
-            `Rewrote Slack URL ${urlObj.href} to deep link ${outputStr}`
-          );
-
-          return new URL(outputStr);
-        }
-      }
-    }
-
-    return urlObj;
-  },
-};
-
-const workPages = [
-  "github.com",
-  "*.auctionet.*",
-  ""
-]
+const openInChrome = [
+  { match: "linear.app/*" },
+  { match: "auctionet.slack.com/*" },
+  { match: "github.com/barsoom/*" },
+];
 
 export default {
   defaultBrowser: "Vivaldi",
-  rewrite: [slackRewriter],
+  rewrite: [rewriteSlack(slackSubdomainMapping)],
   handlers: [
-    // {
-    //   match: "linear.app/*",
-    //   browser: "Linear",
-    // },
-    {
-      match: ({ url }) => { return url.protocol === "slack" || url.protocol === "slack:" },
-      browser: "Slack"
-    },
+    ...openInChrome.map(m => ({ ...m, browser: "Google Chrome" })),
+    { match: ({ url }) => url.protocol === "slack" || url.protocol === "slack:", browser: "Slack" },
   ],
-}
+};
